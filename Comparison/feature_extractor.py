5cb0b78c189ad7ed52d9332f15653f04b47c37fc
Yinjun Harold JIA <jiayinjunharold@gmail.com>
2022-06-03 15:09:29 +0800

    Update feature_extractor.py

--- Comparison/feature_extractor.py
+++ Comparison/feature_extractor.py
@@ -67,80 +67,31 @@ def dist_feat_legs(mat,ids):
     #.reshape(F,-1)
     return dist
 
-src = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/tracking_result/042815_assay4-track.mat'
-tgt = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/dist_feat_legs/'
+src = './tracking_result/042815_assay4-track.mat'
+tgt = './dist_feat_legs/'
 feat = sio.loadmat(src)
 for x in well2fly.keys():
     f = dist_feat_legs(feat,well2fly[x])
     np.save(tgt+x+'.npy',f)
 
-src = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/tracking_result/042815_assay4-track.mat'
-tgt = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/dist_feat/'
+src = './tracking_result/042815_assay4-track.mat'
+tgt = './dist_feat/'
 feat = sio.loadmat(src)
 for x in well2fly.keys():
     f = dist_feat(feat,well2fly[x])
     np.save(tgt+x+'.npy',f)
 
 
-src = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/tracking_result/JAABA'
-tgt = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/JAABA_feat/'
+src = './tracking_result/JAABA'
+tgt = './JAABA_feat/'
 
 for x in well2fly.keys():
     f = JAABA_feat(src,well2fly[x])
     np.save(tgt+x+'.npy',f)
 
-src = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/tracking_result/042815_assay4-feat.mat'
-tgt = '/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Selfee_revision_1/comparison/flytracker_feat/'
+src = './tracking_result/042815_assay4-feat.mat'
+tgt = './flytracker_feat/'
 feat = sio.loadmat(src)
 for x in well2fly.keys():
     f = flytracker_feat(feat,well2fly[x])
     np.save(tgt+x+'.npy',f)
-
-# def frame_show(img,labels,head,tail,center,l_wing,r_wing):
-#     img = cv2.imread(img) #BGR
-#     img  = cv2.circle(img, np.int32(center[0]),1, (255, 25, 255), -1)
-#     img  = cv2.circle(img, np.int32(head[0]),1, (25, 25, 255), -1)
-#     img  = cv2.circle(img, np.int32(tail[0]),1, (255, 25, 25), -1)
-#     img  = cv2.circle(img, np.int32(l_wing[0]),1, (25, 255, 25), -1)
-#     img  = cv2.circle(img, np.int32(r_wing[0]),1, (25, 255, 25), -1)
-#     img  = cv2.circle(img, np.int32(center[1]),1, (255, 25, 255), -1)
-#     img  = cv2.circle(img, np.int32(head[1]),1, (25, 25, 255), -1)
-#     img  = cv2.circle(img, np.int32(tail[1]),1, (255, 25, 25), -1)
-#     img  = cv2.circle(img, np.int32(l_wing[1]),1, (25, 255, 25), -1)
-#     img  = cv2.circle(img, np.int32(r_wing[1]),1, (25, 255, 25), -1)
-#     img = cv2.resize(img,(500,500))
-#     img = cv2.putText(img,labels,(100,490),cv2.FONT_HERSHEY_SIMPLEX,2,(110,110,110),3)
-#     cv2.imshow("Image",img)
-#     #cv2.waitKey(0)
-#     k = cv2.waitKey(50)
-#     if k == 32:
-#         cv2.waitKey(0)
-#     return 0 
-
-# home ='/home/harold/Documents/Harold_D206PC_Data/ZW_Lab/Flyformer/Cortship_Pictures/Fly_vs_fly/Courtship/Courtship/wild'
-# src = 'movie1'
-
-# action = sio.loadmat(os.path.join(home,src,src+'_actions.mat'))
-# #generate labels
-
-
-# feat = sio.loadmat(os.path.join(home,src,src+'_feat.mat'))
-
-# track = sio.loadmat(os.path.join(home,src,src+'_track.mat'))['trk'][0][0][2].transpose(1,0,2)
-# #generate coordinate
-# center = track[:,:,0:2]
-# cos_theta = np.cos(track[:,:,2])*track[:,:,3]/2
-# sin_theta = np.sin(track[:,:,2])*track[:,:,3]/2
-# vec = np.stack([cos_theta,-sin_theta],axis=-1)
-# head = center+vec
-# tail = center-vec
-# l_wing = track[:,:,5:7]
-# r_wing = track[:,:,7:9]
-
-# imgdir = os.path.join(home+'_img',src)
-
-# for i in range(len(os.listdir(imgdir))):
-#     print(i)
-#     frame_show(os.path.join(imgdir,str(i+1)+'.jpg'),head[i],tail[i],center[i],l_wing[i],r_wing[i])
-    
-
